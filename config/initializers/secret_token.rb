5db0948960a0b430cbfa86f1572c8a0e554fccfd
Nelson Pato Jimenez <developer.jimenez@gmail.com>
2014-05-01 15:54:18 -0400

    Inicio del proyecto

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SquapeCom::Application.config.secret_token = '5d0ee420424db49fa57d68ae634236fa8c8a7a2332dfabe16477ca502daef33aa210f56643fb077cf5ee039219bc5e21b1572bf2972cf64c64f56794cb0f2c30'
