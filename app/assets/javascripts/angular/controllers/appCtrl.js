3d5b6abf7d69d59dcf52bdb9dc03d0697a75953d
Nelson Pato Jimenez <developer.jimenez@gmail.com>
2014-05-15 18:23:38 -0400

    Add changes in angular

--- app/assets/javascripts/angular/controllers/appCtrl.js
+++ app/assets/javascripts/angular/controllers/appCtrl.js
@@ -3,12 +3,6 @@ var app = angular.module("appCtrl", []);
 app.controller('HomeCtrl', ['$scope', function($scope) {
   $scope.example = 'Hola!';
   $scope.isMenu = true;
-  $scope.isMap = function(){
-    if ($scope.isMenu){
-      return true;
-    }
-  };
-
 
 
 
