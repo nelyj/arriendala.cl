4eaf8d5564a126ffd6d1099449253e79954d2ee5
Nelson Pato Jimenez <developer.jimenez@gmail.com>
2014-05-21 18:02:05 -0400

    Fixed error in publications form, instead publications_path i'm using publicaciones_path

--- app/models/publication.rb
+++ app/models/publication.rb
@@ -1,5 +1,5 @@
 class Publication < ActiveRecord::Base
-  attr_accessible :description, :title
+  attr_accessible :description, :title, :address, :latitude, :longitude
 
   belongs_to :user
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140506174330) do
+ActiveRecord::Schema.define(:version => 20140521005726) do
 
   create_table "people", :force => true do |t|
     t.string   "name"
@@ -37,6 +37,9 @@ ActiveRecord::Schema.define(:version => 20140506174330) do
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
     t.integer  "user_id"
+    t.string   "address"
+    t.float    "latitude"
+    t.float    "longitude"
   end
 
   add_index "publications", ["user_id"], :name => "index_publications_on_user_id"
