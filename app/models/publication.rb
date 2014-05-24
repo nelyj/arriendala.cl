6d6f5722e8bdf54072be28460b3069f744a53b99
Nelson Pato Jimenez <developer.jimenez@gmail.com>
2014-05-24 13:55:54 -0400

    Add geocoder in publication model

--- app/models/publication.rb
+++ app/models/publication.rb
@@ -2,6 +2,8 @@ class Publication < ActiveRecord::Base
   attr_accessible :description, :title,
   :address, :latitude, :longitude
 
-
+  geocoded_by :address
+  after_validation :geocode
+  
   belongs_to :user
 end
