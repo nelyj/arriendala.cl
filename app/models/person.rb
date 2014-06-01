304e43c756450ec287152d2693d11b2019e3f249
Nelson Pato Jimenez <developer.jimenez@gmail.com>
2014-06-01 02:41:21 -0400

    Add geocoded by live and by work

--- app/models/person.rb
+++ app/models/person.rb
@@ -2,6 +2,10 @@ class Person < ActiveRecord::Base
   attr_accessible :name, :last_name, :address, :latitude, :longitude,
   :address_work, :longitude_work, :latitude_work, :gender, :married, :childrens, :birthday_day, :birthday_month, :birthday_year
 
+  #to live
+  geocoded_by :address
+ 	#to work
+  geocoded_by :address_work, :latitude => :latitude_work, :longitude => :longitude_work
 
   belongs_to :user
 end
