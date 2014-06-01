086ad863fe911792ed8d73bd52805bb28446709c
Nelson Pato Jimenez <developer.jimenez@gmail.com>
2014-06-01 02:39:18 -0400

    Add recent attributes in the person model

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,6 +1,6 @@
 class Person < ActiveRecord::Base
   attr_accessible :name, :last_name, :address, :latitude, :longitude,
-  :birthday_day, :birthday_month, :birthday_year
+  :address_work, :longitude_work, :latitude_work, :gender, :married, :childrens, :birthday_day, :birthday_month, :birthday_year
 
 
   belongs_to :user
